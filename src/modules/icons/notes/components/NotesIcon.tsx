82e72f8fc1a0d4e2e66d756fa36b50fcbedc9e93
Egiazarian Victor <victor.egiazarian@siemens.com>
2019-12-16 10:29:03 +0300

    Add font

--- src/modules/icons/notes/components/NotesIcon.tsx
+++ src/modules/icons/notes/components/NotesIcon.tsx
@@ -13,9 +13,9 @@ export class NotesIcon extends React.PureComponent {
                 <path
                     d="M9 3.93752V15.75M9 3.93752C9 3.93752 5.0625 0.56252 1.125 3.37502V15.75C5.0625 12.9375 9 15.75 9 15.75C9 15.75 12.9375 12.9375 16.875 15.75V3.37502C12.9375 0.56252 9 3.93752 9 3.93752Z"
                     stroke="#ADADBE"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
             </svg>
         );
